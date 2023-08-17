e38ce39778037a318ef9b95a8c83e637d516d7cc
German Drusetich <germandrusetich@gmail.com>
2023-08-17 17:12:49 -0300

    Agregando archivo persistidor

--- src/main/java/com/accuweather/FalsoAccuWeather.java
+++ src/main/java/com/accuweather/FalsoAccuWeather.java
@@ -1,21 +1,21 @@
 package com.accuweather;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import ar.edu.utn.frba.dds.Temperatura;
 
-public class FalsoAccuWeather implements ServicioMeteorologico{
-AccuWeatherAPI apiFalsa = new AccuWeatherAPI();
-
+public class FalsoAccuWeather implements ServicioMeteorologico {
+	AccuWeatherAPI apiFalsa = new AccuWeatherAPI();
 
 	Temperatura obtenerTemperaturaActualEnLaCiudad(String unaCiudad) {
-		
-		List<Map<String, Object>> condicionesClimaticas = apiFalsa.getWeather(unaCiudad);
-		Object temperaturaActual = condicionesClimaticas.get(0).get("Temperature");
-		
-		return temperaturaActual.;
+
+		List<Map<String, Temperatura>> condicionesClimaticas = apiFalsa.getWeather(unaCiudad);
+		Temperatura temperaturaActual = condicionesClimaticas.get(0).get("Temperature");
+
+		return temperaturaActual;
 
 	}
-	
+
 }
--- src/test/java/ar/edu/utn/frba/dds/AccuWeatherAPITest.java
+++ src/test/java/ar/edu/utn/frba/dds/AccuWeatherAPITest.java
@@ -2,6 +2,7 @@ package ar.edu.utn.frba.dds;
 
 import com.accuweather.*;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,6 +14,11 @@ public class AccuWeatherAPITest {
 
 	@Test
 	void sePuedeConsultarAAccuWeatherElTiempoDeBuenosAires() {
+		List<Map<String, Object>> clima = Arrays.asList(new HashMap<String, Object>(){
+			put("Value", 57);
+			put("Unit", "F");
+			put("UnitType", 18);
+		});
 		AccuWeatherAPI apiClima = new AccuWeatherAPI();
 		List<Map<String, Object>> estadoDelTiempo = apiClima.getWeather("Buenos Aires, Argentina");  
 		Assertions.assertEquals(estadoDelTiempo.get(0).get("Temperature"),new HashMap<String, Object>()); 
